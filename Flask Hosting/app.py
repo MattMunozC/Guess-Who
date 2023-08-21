317d3b933dc102aec59a27893301865f7e0c2a3f
Matias Muñoz <matias.munoz.coquidan@gmail.com>
2023-08-21 10:42:42 -0400

    resources endpoint created

--- Flask Hosting/app.py
+++ Flask Hosting/app.py
@@ -1,14 +1,26 @@
 #!/usr/bin/python
 # -*- coding: ascii -*-
 from unicodedata import category
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect,send_file
+from pathlib import Path
+from os import path
+
 
 app = Flask(__name__)
+BASE_DIR = Path(__file__).resolve().parent
+STATIC_FILE_DIR=path.join(BASE_DIR,"static")
+MEDIA_FILE_DIR=path.join(BASE_DIR,"media")
+
 
 @app.route('/')
 def Index():
     return render_template("home.html")
+@app.route('/static/<type>/<resource>',methods=['GET'])
+def resource_retrieve(type,resource):
+    return send_file(f"{STATIC_FILE_DIR}/{type}/{resource}")
+
 if __name__=="__main__":
     #PORT=1521 HOST=LOCALHOST
     app.debug=True
-    app.run("192.168.1.86")
+    app.run()
+
